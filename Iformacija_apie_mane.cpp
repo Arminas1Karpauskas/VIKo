7f95b0c98daf1fa592e4b709cd074b7310596068
Arminas1Karpauskas <arminaskarpauskas2@gmail.com>
2024-10-02 11:22:52 +0300

    1uzd
    Pratybos rugsėjo 12-13 dienomis

---
+++ Iformacija_apie_mane.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Studentų informacija
+    string vardas = "Arminas";
+    string pavarde = "Karpauskas";
+    int amzius = 21;
+    string grupe = "PI24";
+    int kursas = 1;
+    string studiju_programa = "Programu Sistemos";
+
+    // Išvedimas
+    cout << "Studentas: " << vardas << " " << pavarde << endl;
+    cout << "Amzius: " << amzius << endl;
+    cout << "Grupe: " << grupe << endl;
+    cout << "Kursas: " << kursas << endl;
+    cout << "Studiju programa: " << studiju_programa << endl;
+
+    return 0;
+}
