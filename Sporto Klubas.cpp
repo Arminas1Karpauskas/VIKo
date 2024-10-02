ce5db783fda8a632c38ce22be17f6377f4c7fbe8
Arminas1Karpauskas <arminaskarpauskas2@gmail.com>
2024-10-02 11:27:39 +0300

    Sporto Klubas
    Pratybos rugsėjo 12-13 dienomis. 2 Uzd.

---
+++ Sporto Klubas.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Sporto klubo informacija
+    string pavadinimas = "Kauno Zalgiris";
+    int ikurimo_metai = 1944;
+    string savininkas = "Kauno miestas";
+    string arena = "Zalgirio arena";
+    int vietu_skaicius = 15400;
+
+    // Išvedimas
+    cout << "Sporto klubas: " << pavadinimas << endl;
+    cout << "Ikurimo metai: " << ikurimo_metai << endl;
+    cout << "Savininkas: " << savininkas << endl;
+    cout << "Arena: " << arena << endl;
+    cout << "Vietu skaicius: " << vietu_skaicius << endl;
+
+    return 0;
+}
